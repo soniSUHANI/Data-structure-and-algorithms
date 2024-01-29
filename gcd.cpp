4bd89e4ebeccd89eba1d55df1729dc75c82c0eb8
soni <114800693+soniSUHANI@users.noreply.github.com>
2024-01-30 01:30:28 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a,int b){
+	if(a==0){
+		return b;
+	}
+	if(b==0){
+		return a;
+	}
+	while(a!=b){
+		if(a>b){
+			a=a-b;
+		}
+		else{
+			b=b-a;
+		}
+	}
+	return a;
+}
+
+int main(){
+	int a,b;
+	cout<<"Enter the number a and b ";
+	cin>>a>>b;
+	
+	int ans = gcd(a,b);
+	cout<<"The gcd of a and b are "<<ans<<endl; 
+}
